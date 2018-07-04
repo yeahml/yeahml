92dc362d75b640709b88e07dd8a2da385d6dedc2
Jack Burdick <jackbburdick@gmail.com>
2018-07-04 09:08:58 -0400

    Move init from file logic into a separate file
    I'd like to keep the train_graph logic concise and the logic for loading params from files is lengthy and little messy and accomplishes a single task - for this reason, I decided it would be better to move it to it's own file where the focus can be on only initializing the weights

--- train_graph.py
+++ train_graph.py
@@ -9,12 +9,15 @@ from handle_data import return_batched_iter
 
 # from helper import load_obj, save_obj, get_model_params
 
+# used for loading params
+from load_params_onto_layer import init_params_from_file
 
-def train_graph(g, MCd, HCd):
+
+def train_graph(g, MCd: dict, HCd: dict):
 
     EARLY_STOPPING_e = MCd["early_stopping_e"]  # default is preset to 0
     WARM_UP_e = MCd["warm_up_epochs"]  # default is 3
-    FULL_ERROR = MCd["full_error"]
+    # FULL_ERROR = MCd["full_error"]
 
     init_global, init_local = g.get_collection("init")
     X, y_raw, training, training_op = g.get_collection("main_ops")
@@ -51,15 +54,9 @@ def train_graph(g, MCd, HCd):
         sess.run([init_global, init_local])
         saver = tf.train.Saver()  # create after initializing variables
 
-        # ================= transfer learning
-        # TODO: if a special path is listed to load vars from for a particular layer,
-        # a second init is needed. I don't think this use case will be very common
-        # so I will come back and work on this logic later
-
-        # INIT_DEBUG
+        ## TL, INIT_DEBUG, pre loading
         # print("TRANSFER INIT : {}".format(MCd["load_params_path"]))
-        # AA = 1
-        # BB = 0
+        # AA, BB = 1, 0
         # dense_1_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="dense_1")
         # dense_1_vars = [
         #     v
@@ -67,94 +64,21 @@ def train_graph(g, MCd, HCd):
         #     if v.name.rstrip("0123456789").endswith("kernel:")
         #     or v.name.rstrip("0123456789").endswith("bias:")
         # ]
-        # print(dense_1_vars)
+        # # print(dense_1_vars)
         # d1_pre = sess.run(dense_1_vars)
         # print(d1_pre[AA][BB])
 
-        load_names, layer_tensor_params = [], []
-        for i, l_name in enumerate(HCd["layers"]):
-            try:
-                if HCd["layers"][l_name]["saver"]["load_params"]:
-                    # set name to load var from the indicated path, will default to
-                    # the current name of the layer
-                    try:
-                        load_name = HCd["layers"][l_name]["saver"]["load_name"]
-                        if load_name == None:
-                            load_name = l_name
-                    except KeyError:
-                        load_name = l_name
-                    # set the path from which to load the variables. The default path
-                    # is set in the model config but an option is presented to load from other files
-                    # try:
-                    #    load_path = HCd["layers"][l_name]["saver"]["load_path"]
-                    # except KeyError:
-                    #    load_name = MCd["load_params_path"]
+        # initialize variables from file as specified (as used in transfer learning)
+        # TODO: I would rather this have a successful return, rather than act as a method
+        init_params_from_file(sess, MCd, HCd)
 
-                    try:
-                        name_str = "{}".format(l_name)
-                        layer_tensor = tf.get_collection(
-                            tf.GraphKeys.GLOBAL_VARIABLES, scope=name_str
-                        )
-                        # filter for only bias or kernel
-                        layer_tensor = [
-                            l
-                            for l in layer_tensor
-                            if l.name.rstrip("0123456789").endswith("kernel:")
-                            or l.name.rstrip("0123456789").endswith("bias:")
-                        ]
-
-                        for t_param in layer_tensor:
-                            # the split logic is used to remove the (potentially different) name
-                            # for example conv_2/kernel:0 will become kernel:0 which will become "kernel", for which we can
-                            # append to the name to be used for the layer
-                            p_name = t_param.name.split("/")[1].split(":")[0]
-
-                            # build list of vars to load and vars to load onto
-                            load_names.append(load_name + "/" + p_name)
-                            layer_tensor_params.append(t_param)
-
-                        # layer_tensor = g.get_tensor_by_name("{}:{}".format(l_name))
-                    except:
-                        sys.exit("unable to get {}".format(l_name))
-
-                    # print("let's do some TL on {}".format(HCd["layers"][l_name]))
-            except KeyError:
-                # no init from saver
-                pass
-
-        assert len(load_names) == len(
-            layer_tensor_params
-        ), "indicated number of params to load and params found are not equal"
-
-        ## Initialize indicated vars from file
-        init_vars = dict(zip(load_names, layer_tensor_params))
-        if len(init_vars) > 0:
-            restore_saver = tf.train.Saver(init_vars)
-            try:
-                restore_saver.restore(sess, MCd["load_params_path"])
-            except tf.errors.InvalidArgumentError as err:
-                if FULL_ERROR:
-                    print(err)
-                else:
-                    print(err.message)
-                    print(
-                        "The likely cause of this error is:\n 1) {}\n 2) {}".format(
-                            "The shapes are mismathed (check err message for hint)",
-                            "The naming of the target tensor is mismatched",
-                        )
-                    )
-                    print(
-                        "note: if you wish to see the full error message, please enable 'overall:full_error_message: True'"
-                    )
-                sys.exit("ERROR > EXIT: unable to restore indicated params")
+        # TL, INIT_DEBUG, post loading
+        # d1_post = sess.run(dense_1_vars)
+        # print(d1_post[AA][BB])
 
         filenames_ph = tf.placeholder(tf.string, shape=[None])
         tr_iter = return_batched_iter("train", MCd, filenames_ph)
         val_iter = return_batched_iter("val", MCd, filenames_ph)
-
-        # INIT_DEBUG
-        # d1_post = sess.run(dense_1_vars)
-        # print(d1_post[AA][BB])
 
         # tracing options
         try:
@@ -172,10 +96,6 @@ def train_graph(g, MCd, HCd):
         except KeyError:
             run_options = None
             pass
-
-        # INIT_DEBUG
-        # yp_post = sess.run(yp_vars)
-        # print(yp_post)
 
         local_step = 0  # This should be an internal tf counter.
         for e in tqdm(range(1, MCd["epochs"] + 1)):
@@ -195,7 +115,6 @@ def train_graph(g, MCd, HCd):
 
             # loop entire training set
             # main training loop
-
             while True:
                 try:
                     local_step += 1
@@ -281,7 +200,7 @@ def train_graph(g, MCd, HCd):
             val_writer.add_summary(summary, e)
             val_writer.flush()
 
-        # INIT_DEBUG
+        # TL INIT_DEBUG, post training
         # d1_post = sess.run(dense_1_vars)
         # print(d1_post[AA][BB])
 
