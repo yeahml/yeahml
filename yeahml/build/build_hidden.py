dec4a96718444038fe9fbc1d29f88897ff56fabe
Jack Burdick <JackBurdick@users.noreply.github.com>
2018-12-19 10:43:40 -0800

    Add support for global+1d pooling
    TODO: confirm that the global 2d pooling works as expected and confirm that the 1d pooling (not global) works as expected. Also, there is no support for global max pooing at this time.

--- yeahml/build/build_hidden.py
+++ yeahml/build/build_hidden.py
@@ -233,7 +233,7 @@ def build_dense_layer(
     return out
 
 
-def build_pool_layer(cur_input, training, opts: dict, name: str, logger, g_logger):
+def build_pool_2d_layer(cur_input, training, opts: dict, name: str, logger, g_logger):
 
     try:
         if opts:
@@ -310,6 +310,156 @@ def build_pool_layer(cur_input, training, opts: dict, name: str, logger, g_logge
     return out
 
 
+def build_global_pooling_layer(
+    cur_input, training, opts: dict, name: str, logger, g_logger
+):
+
+    # TODO: implement global max pooling
+    try:
+        if opts:
+            pool_type = opts["type"]
+        else:
+            pool_type = "avg"
+    except KeyError:
+        pool_type = "avg"
+    logger.debug("pool_type set: {}".format(pool_type))
+
+    if pool_type == "avg":
+        # TODO: this 2 is hard coded
+        # in a 2d situation, this would be [1,2]
+        if cur_input.get_shape().ndims == 3:
+            out = tf.reduce_mean(cur_input, [2], name=name)
+        elif cur_input.get_shape().ndims == 4:
+            out = tf.reduce_mean(cur_input, [1,2], name=name)
+    else:
+        sys.exit("pool type {} is not allowed".format(pool_type))
+    logger.debug("tensor obj: {}".format(out))
+    g_logger.info("{}".format(fmt_tensor_info(out)))
+    logger.debug("[End] building: {}".format(name))
+
+    return out
+
+
+def build_pool_1d_layer(cur_input, training, opts: dict, name: str, logger, g_logger):
+
+    try:
+        if opts:
+            pool_size = opts["pool_size"]
+        else:
+            pool_size = 3
+    except KeyError:
+        pool_size = 3
+    logger.debug("pool_size set: {}".format(pool_size))
+
+    try:
+        if opts:
+            strides = opts["strides"]
+        else:
+            strides = 3
+    except KeyError:
+        strides = 3
+    logger.debug("strides set: {}".format(strides))
+
+    if not name:
+        name = "unnamed_pool1d_layer"
+    logger.debug("name set: {}".format(name))
+
+    try:
+        if opts:
+            pool_type = opts["pool_type"]
+        else:
+            pool_type = "max"
+        if pool_type not in ["max", "avg"]:
+            sys.exit("pool type {} is not allowed".format(pool_type))
+    except KeyError:
+        pool_type = "max"
+    logger.debug("pool_type set: {}".format(pool_type))
+
+    if pool_type == "max":
+        out = tf.layers.max_pooling1d(
+            cur_input, pool_size=pool_size, strides=strides, padding="same", name=name
+        )
+    elif pool_type == "avg":
+        out = tf.layers.average_pooling1d(
+            cur_input, pool_size=pool_size, strides=strides, padding="same", name=name
+        )
+    else:
+        # for future pooling implementations
+        sys.exit("pool type {} is not yet implemented".format(pool_type))
+
+    logger.debug("tensor obj pre dropout: {}".format(out))
+    g_logger.info("{}".format(fmt_tensor_info(out)))
+
+    ## add dropout, if indicated
+    try:
+        if opts:
+            dropout_rate = opts["dropout"]
+        else:
+            dropout_rate = None
+    except KeyError:
+        dropout_rate = None
+    logger.debug("dropout_rate set: {}".format(dropout_rate))
+
+    if dropout_rate:
+        out = tf.layers.dropout(
+            inputs=out,
+            rate=dropout_rate,
+            noise_shape=None,
+            seed=None,
+            training=training,
+            name=None,
+        )
+        logger.debug("tensor obj post dropout: {}".format(out))
+        g_logger.info(">> dropout: {}".format(dropout_rate))
+
+    logger.debug("[End] building: {}".format(name))
+
+    return out
+
+
+def build_embedding_layer(cur_input, training, opts: dict, name: str, logger, g_logger):
+
+    # TODO: the default vocabulary_size size does not make sense here
+    try:
+        if opts:
+            vocabulary_size = opts["vocabulary_size"]
+        else:
+            vocabulary_size = 100
+    except KeyError:
+        vocabulary_size = 100
+    logger.debug("vocabulary_size set: {}".format(vocabulary_size))
+
+    # TODO: the default embedding_size size does not make sense here
+    try:
+        if opts:
+            embedding_size = opts["embedding_size"]
+        else:
+            embedding_size = 2
+    except KeyError:
+        embedding_size = 2
+    logger.debug("embedding_size set: {}".format(embedding_size))
+
+    if not name:
+        name = "unnamed_embedding_layer"
+    logger.debug("name set: {}".format(name))
+
+    # TODO: I'm not sure this implemented correctly
+    word_embeddings = tf.get_variable(
+        "word_embeddings", [vocabulary_size, embedding_size]
+    )
+    embedded_word_ids = tf.nn.embedding_lookup(word_embeddings, cur_input)
+
+    out = embedded_word_ids
+
+    logger.debug("tensor obj pre dropout: {}".format(out))
+    g_logger.info("{}".format(fmt_tensor_info(out)))
+    logger.debug("[End] building: {}".format(name))
+
+    # TODO: VIZUALIZING EMBEDDINGS - https://www.tensorflow.org/guide/embedding
+
+    return out
+
+
 def build_hidden_block(X, training, MCd: dict, HCd: dict, logger, g_logger):
     # logger = config_logger(MCd, "build")
     logger.info("-> START building hidden block")
@@ -364,7 +514,11 @@ def build_hidden_block(X, training, MCd: dict, HCd: dict, logger, g_logger):
             # or conv, we need to flatten layer before we add a dense layer
             prev_ltype_key = list(HCd["layers"])[i - 1]
             prev_ltype = HCd["layers"][prev_ltype_key]["type"]
-            if prev_ltype == "conv2d" or prev_ltype == "pooling2d":
+            if (
+                prev_ltype == "conv2d"
+                or prev_ltype == "pooling2d"
+                # or prev_ltype == "pooling1d"
+            ):
                 # flatten
                 # maybe remove np dependency cur_input.get_shape().as_list()[1:]
                 last_shape = int(np.prod(cur_input.get_shape()[1:]))
@@ -377,7 +531,22 @@ def build_hidden_block(X, training, MCd: dict, HCd: dict, logger, g_logger):
             )
         elif ltype == "pooling2d":
             logger.debug("-> START building: {}".format(ltype))
-            cur_input = build_pool_layer(
+            cur_input = build_pool_2d_layer(
+                cur_input, training, opts, l_name, logger, g_logger
+            )
+        elif ltype == "pooling1d":
+            logger.debug("-> START building: {}".format(ltype))
+            cur_input = build_pool_1d_layer(
+                cur_input, training, opts, l_name, logger, g_logger
+            )
+        elif ltype == "global_pooling":
+            logger.debug("-> START building: {}".format(ltype))
+            cur_input = build_global_pooling_layer(
+                cur_input, training, opts, l_name, logger, g_logger
+            )
+        elif ltype == "embedding":
+            logger.debug("-> START building: {}".format(ltype))
+            cur_input = build_embedding_layer(
                 cur_input, training, opts, l_name, logger, g_logger
             )
         else:
