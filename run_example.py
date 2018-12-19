7acaceb9812939a6a3465a161ac514800a57166a
Jack Burdick <JackBurdick@users.noreply.github.com>
2018-12-19 10:42:12 -0800

    Include sentiment analysis example

--- run_example.py
+++ run_example.py
@@ -12,11 +12,12 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 ## Config files for currently supported modes
 # example = "./examples/cats_v_dogs_01/model_config.yaml"  # sigmoid example
 # example = "./examples/mnist/model_config.yaml"  # softmax example
-example = "./examples/cali_housing/model_config.yaml"  # regression example
+# example = "./examples/cali_housing/model_config.yaml"  # regression example
 # example = "./examples/segmentation/model_config.yaml"  # binary segmentation example
 # example = (
 #     "./examples/multi_segmentation/model_config.yaml"
 # )  # multi segmentation example
+example = "./examples/sentiment_imdb/model_config.yaml"  # sentiment analysis example
 model_config, hidden_config = yml.create_model_and_hidden_config(example)
 
 
