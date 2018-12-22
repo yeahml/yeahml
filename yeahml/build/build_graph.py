71d699ed29bf0c7f41903f803efc1375cd024268
Jack Burdick <JackBurdick@users.noreply.github.com>
2018-12-21 21:53:12 -0800

    Remove layer_names logic
    a placeholder function has been added to helper.py and could eventually hold logic to create more readable layer names, but for now, this is an easier solution.. In the new logic, names for the layers are created based on the names of the tensors in the bias params

--- yeahml/build/build_graph.py
+++ yeahml/build/build_graph.py
@@ -275,7 +275,6 @@ def build_graph(MCd: dict, HCd: dict):
         logger.debug("create scalar bias: {}".format(weights))
         assert len(weights) == len(bias), "number of weights & bias are not equal"
         logger.debug("len(weights) == len(bias) = {}".format(len(weights)))
-        layer_names = list(HCd["layers"])
 
         weights.append(
             [
@@ -285,78 +284,15 @@ def build_graph(MCd: dict, HCd: dict):
             ][0]
         )
 
-        # TODO: hardcoded way of removing logits from segmentation development
-        if (
-            MCd["loss_fn"] != "softmax_binary_segmentation_temp"
-            and MCd["loss_fn"] != "softmax_multi_segmentation_temp"
-        ):
-            layer_names.append("logits")
-        # exclude all pooling layers
-        # TODO: this logic assumes that the layer name corresponds to the type of layer
-        # > ideally, this list should be built by inspecting the layer 'type', but for beta
-        # > purposes, this works for now.
-        layer_names = [
-            l
-            for l in layer_names
-            if not l.startswith("pool") and not l.startswith("embedding")
-        ]
-        inds_to_add = []
-        for i, l in enumerate(layer_names):
-            if l.endswith("rnn"):
-                try:
-                    if HCd["layers"][l]["options"]["condense_out"]:
-                        inds_to_add.append(i + 1)
-                except KeyError:
-                    pass
+        # TODO: this is ugly.... and needs to be improved
+        # a placeholder function has been placed in helper (create_layer_names)
+        # that could help replace this.
+        # layer_names = create_layer_names(list(HCd["layers"]))
+        layer_names = [b.name.replace("/", "__").replace(":", "_") for b in bias]
 
-        # j is needed to ensure the indexes don't become incorrect
-        # after the insertion of each new piece
-        j = 0
-        for i in inds_to_add:
-            layer_names.insert(i + j, "condense_rnn")
-            j += 1
-
-        # bidirectional RNN
-        inds_to_add = []
-        for i, l in enumerate(layer_names):
-            if l.endswith("rnn"):
-                try:
-                    if HCd["layers"][l]["options"]["bidirectional"]:
-                        inds_to_add.append(i + 1)
-                except KeyError:
-                    pass
-        # j is needed to ensure the indexes don't become incorrect
-        # after the insertion of each new piece
-        j = 0
-        for i in inds_to_add:
-            layer_names[i - 1] = "bi_rnn_fw"
-            layer_names.insert(i + j, "bi_rnn_bw")
-            j += 1
-
-        # multi-layer RNN
-        inds_to_add = []
-        for i, l in enumerate(layer_names):
-            if l.endswith("rnn"):
-                try:
-                    num_layers = HCd["layers"][l]["options"]["num_layers"]
-                    if num_layers > 1:
-                        inds_to_add.append([i + 1, num_layers])
-                except KeyError:
-                    pass
-        # j is needed to ensure the indexes don't become incorrect
-        # after the insertion of each new piece
-        j = 0
-        for t in inds_to_add:
-            i = t[0]
-            for v in range(t[1]):
-                # add to same index t[1] number of times
-                if v == 0:
-                    layer_names[i - 1] = "rnn_layer_{}".format(v + 1)
-                else:
-                    layer_names.insert(i, "rnn_layer_{}".format(v + 1))
-                    j += 1
-
-        assert len(bias) == len(layer_names), "num of bias not equal to num layers"
+        assert len(bias) == len(
+            layer_names
+        ), "number of bias params not equal to number of layers"
 
         hist_list = []
         for i, l_weight in enumerate(weights):
