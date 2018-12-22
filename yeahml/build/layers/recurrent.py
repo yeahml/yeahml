318f4f42d3bee45a085f7de8a5ae8489401eb6f6
Jack Burdick <JackBurdick@users.noreply.github.com>
2018-12-21 19:56:45 -0800

    Automatically handle rank error w/input to recurrent
    recurrent layers require the input to be rank > 3 and this will automatically apply a expand_dims (and squeeze) if it seems necessary. by "seems" I mean if the rank is < 3 and the last dimension is != 1. this allows for adding and removing a dim easily. This may not be perfect, but it does seem to work in this instance. e.g. (None, 256) will become (None, 256, 1) and then will become (None, 256) again.

--- yeahml/build/layers/recurrent.py
+++ yeahml/build/layers/recurrent.py
@@ -1,3 +1,4 @@
+import sys
 import tensorflow as tf
 from yeahml.helper import fmt_tensor_info
 from yeahml.build.layers.dense import build_dense_layer
@@ -7,10 +8,37 @@ def build_recurrent_layer(
     cur_input, training, opts: dict, actfn, name: str, logger, g_logger
 ):
 
-    # print(cur_input)
-    cur_input = tf.expand_dims(cur_input, -1)
-    # print("**" * 10)
-    # print(cur_input)
+    # Check the current input and whether a dim needs to be added
+    # e.g. (None, 256) -> (None, 256, 1)
+    # NOTE: not sure if get_shape() or .shape is better
+    REMOVE_DIM: bool = False
+    cur_input_rank = len(cur_input.get_shape())
+
+    if cur_input_rank < 3:
+        # possibly need to add a dim (will need to then remove at the end)
+        if cur_input.get_shape()[-1] > 1:
+            logger.debug(
+                "cur_input shape: ({}) in `build_recurrent_layer`".format(
+                    cur_input.get_shape()
+                )
+            )
+            cur_input = tf.expand_dims(cur_input, -1)
+            logger.debug(
+                "cur_input reshaped: ({}) in `build_recurrent_layer`".format(
+                    cur_input.get_shape()
+                )
+            )
+            REMOVE_DIM = True
+        else:
+            sys.exit(
+                "The current input is shape: ({}) and is required to be rank > 3".format(
+                    cur_input.get_shape()
+                )
+            )
+            # this would mean the input is (NONE, 1)... and would need to become
+            # (None, 1, 1) and I'm not sure this is the case, especially when
+            # dealing with an RNN
+
     # TODO: this will need to broken down much further
     # - different cell types
     # - bidirectional
@@ -118,18 +146,23 @@ def build_recurrent_layer(
     out = rnn_io
     logger.debug("tensor obj: {}".format(out))
     logger.debug("[End] building: {}".format(name))
-    # TODO: UGH
-    # print("**" * 10)
-    # print(out)
+
     if dense_ops:
         out = build_dense_layer(
             out, training, dense_ops, None, "rnn_condense", logger, g_logger
         )
-    # print("**" * 10)
-    # print(out)
-    out = tf.squeeze(out, -1)
+
+    logger.debug(
+        "out in output shape: ({}) in `build_recurrent_layer`".format(out.get_shape())
+    )
+
+    if REMOVE_DIM:
+        out = tf.squeeze(out, -1)
+        logger.debug(
+            "out reshaped to final output shape: ({}) in `build_recurrent_layer`".format(
+                out.get_shape()
+            )
+        )
     g_logger.info("{}".format(fmt_tensor_info(out)))
-    # print("**" * 10)
-    # print(out)
 
     return out
