ceba0dfcec2df0b10b80df87bfdc0c2593172d8c
Jack Burdick <jackbburdick@gmail.com>
2019-04-27 23:04:33 -0700

    include deconv layer
    and update .build_layers to f-strings

--- yeahml/build/build_layers.py
+++ yeahml/build/build_layers.py
@@ -14,6 +14,7 @@ from yeahml.build.layers.pooling import (
 
 # conv layers
 from yeahml.build.layers.convolution import build_conv_layer
+from yeahml.build.layers.deconvolution import build_deconv_layer
 from yeahml.build.layers.recurrent import build_recurrent_layer
 from yeahml.build.layers.dense import build_dense_layer
 from yeahml.build.layers.other import (
@@ -43,63 +44,58 @@ def build_hidden_block(MCd: dict, HCd: dict, logger, g_logger) -> List[Any]:
         actfn = get_activation_fn(layer_info["activation"])
 
         ltype = layer_info["type"].lower()
-        # if ltype == "conv2d":
-        #     logger.debug("-> START building: {}".format(ltype))
-        #     cur_layer = build_conv2d_layer(opts, actfn, l_name, logger, g_logger)
-        if ltype == "deconv2d":
-            logger.debug("-> START building: {}".format(ltype))
-            # cur_input = build_conv2d_transpose_layer(
-            #     cur_input, opts, actfn, l_name, logger, g_logger
-            # )
-            raise NotImplementedError
-        elif ltype == "conv":
+        if ltype == "conv":
+            logger.debug(f"-> START building: {ltype}")
             cur_layer = build_conv_layer(opts, actfn, l_name, logger, g_logger)
+        elif ltype == "deconv" or ltype == "conv_transpose":  # TODO: simplify
+            logger.debug(f"-> START building: {ltype}")
+            cur_input = build_deconv_layer(opts, actfn, l_name, logger, g_logger)
         elif ltype == "flatten":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             cur_layer = tf.keras.layers.Flatten()
         elif ltype == "dense":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # TODO: need to flatten?
             cur_layer = build_dense_layer(opts, actfn, l_name, logger, g_logger)
         elif ltype == "pooling2d":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             cur_layer = build_pool_2d_layer(opts, l_name, logger, g_logger)
         elif ltype == "pooling1d":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # cur_input = build_pool_1d_layer(
             #     cur_input, training, opts, l_name, logger, g_logger
             # )
             raise NotImplementedError
         elif ltype == "global_pooling":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # cur_input = build_global_pooling_layer(
             #     cur_input, training, opts, l_name, logger, g_logger
             # )
             raise NotImplementedError
         elif ltype == "embedding":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # cur_input = build_embedding_layer(
             #     cur_input, training, opts, l_name, logger, g_logger
             # )
             raise NotImplementedError
         elif ltype == "batch_normalization":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # cur_input = build_batch_normalization_layer(
             #     cur_input, training, opts, l_name, logger, g_logger
             # )
             raise NotImplementedError
         elif ltype == "recurrent":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             # cur_input = build_recurrent_layer(
             #     cur_input, training, opts, actfn, l_name, logger, g_logger
             # )
             raise NotImplementedError
         elif ltype == "dropout":
-            logger.debug("-> START building: {}".format(ltype))
+            logger.debug(f"-> START building: {ltype}")
             cur_layer = build_dropout_layer(opts, l_name, logger, g_logger)
         else:
-            logger.fatal("unable to build layer type: {}".format(ltype))
-            sys.exit("unable to build layer type: {}".format(ltype))
+            logger.fatal(f"unable to build layer type: {ltype}")
+            raise NotImplementedError(f"layer type: {ltype} not implemented yet")
 
         HIDDEN_LAYERS.append(cur_layer)
 
--- yeahml/build/layers/deconvolution.py
+++ yeahml/build/layers/deconvolution.py
@@ -5,10 +5,8 @@ from typing import Any
 
 from yeahml.helper import fmt_tensor_info
 
-# NOTE: the default padding is "same", this is different from the API which is "same"
 
-
-def build_conv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
+def build_deconv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
     # TODO: default behavior is w/in the exception block, this may need to change
     # default is 3x3, stride = 1
 
@@ -16,25 +14,25 @@ def build_conv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
         k_init_fn = get_initializer_fn(opts["kernel_initializer"])
     except KeyError:
         k_init_fn = None
-    logger.debug("k_init_fn set: {}".format(k_init_fn))
+    logger.debug(f"k_init_fn set: {k_init_fn}")
 
     try:
         k_reg = get_regularizer_fn(opts["kernel_regularizer"])
     except KeyError:
         k_reg = None
-    logger.debug("k_reg set: {}".format(k_reg))
+    logger.debug(f"k_reg set: {k_reg}")
 
     try:
         b_reg = get_regularizer_fn(opts["bias_regularizer"])
     except KeyError:
         b_reg = None
-    logger.debug("b_reg set: {}".format(b_reg))
+    logger.debug(f"b_reg set: {b_reg}")
 
     try:
         data_format = opts["data_format"]
     except:
         data_format = None
-    logger.debug("data_format set: {}".format(data_format))
+    logger.debug(f"data_format set: {data_format}")
 
     # try:
     #     dilation_rate = opts["dilation_rate"]
@@ -47,18 +45,11 @@ def build_conv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
         padding = opts["padding"]
     except KeyError:
         padding = "SAME"
-    logger.debug("padding set: {}".format(padding))
+    logger.debug(f"padding set: {padding}")
 
     if not name:
         name = "unnamed_conv_layer"
-    logger.debug("name set: {}".format(name))
-
-    # try:
-    #     trainable = opts["trainable"]
-    # except KeyError:
-    #     # trainable by default
-    #     trainable = True
-    # logger.debug("trainable set: {}".format(trainable))
+    logger.debug(f"name set: {name}")
 
     try:
         conv_dim = opts["dimension"]  # TOOD: shorthand 'dim'
@@ -70,7 +61,7 @@ def build_conv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
         filters = opts["filters"]
     except KeyError:
         raise ValueError(f"filters not set for {name}")
-    logger.debug("filters set: {}".format(filters))
+    logger.debug(f"filters set: {filters}")
 
     try:
         kernel_size = opts["kernel_size"]
@@ -85,66 +76,48 @@ def build_conv_layer(opts: dict, actfn, name: str, logger, g_logger) -> Any:
     logger.debug(f"strides set: {strides}")
 
     # TODO: perform check against strides for shape
-    if conv_dim == 1:
-        out = tf.keras.layers.Conv1D(
+    if conv_dim == 2:
+        out = tf.keras.layers.Conv2DTranspose(
             filters,
             kernel_size,
-            strides=strides,
+            strides=strides,  # (1,1)
             padding=padding,
-            data_format=data_format,
-            dilation_rate=1,
+            output_padding=None,
+            data_format=None,
+            dilation_rate=(1, 1),
             activation=actfn,
             use_bias=True,
             kernel_initializer="glorot_uniform",  # TODO: correct, k_init_fn
             bias_initializer="zeros",  # TODO: implement
             kernel_regularizer=k_reg,
-            bias_regularizer=b_reg,  # tf.zeros_initializer()
-            activity_regularizer=None,
-            kernel_constraint=None,
-            bias_constraint=None,
-            name=name,
-        )
-    elif conv_dim == 2:
-        out = tf.keras.layers.Conv2D(
-            filters,
-            kernel_size,
-            strides=strides,
-            padding=padding,
-            data_format=data_format,
-            dilation_rate=(1, 1),  # TODO: implement
-            activation=actfn,
-            use_bias=True,
-            kernel_initializer="glorot_uniform",  # TODO: correct, k_init_fn
-            bias_initializer="zeros",  # TODO: implement
-            kernel_regularizer=k_reg,
-            bias_regularizer=b_reg,  # tf.zeros_initializer()
+            bias_regularizer=b_reg,
             activity_regularizer=None,
             kernel_constraint=None,
             bias_constraint=None,
             name=name,
         )
     elif conv_dim == 3:
-        out = tf.keras.layers.Conv3D(
+        out = tf.keras.layers.Conv3DTranspose(
             filters,
             kernel_size,
-            strides=strides,
+            strides=strides,  # (1,1,1)
             padding=padding,
-            data_format=data_format,
-            dilation_rate=(1, 1, 1),
-            activation=None,
+            output_padding=None,
+            data_format=None,
+            activation=actfn,
             use_bias=True,
-            kernel_initializer="glorot_uniform",
-            bias_initializer="zeros",
-            kernel_regularizer=None,
-            bias_regularizer=None,
+            kernel_initializer="glorot_uniform",  # TODO: correct, k_init_fn
+            bias_initializer="zeros",  # TODO: implement
+            kernel_regularizer=k_reg,
+            bias_regularizer=b_reg,
             activity_regularizer=None,
             kernel_constraint=None,
             bias_constraint=None,
             name=name,
         )
     else:
-        raise ValueError(f"conv_dim {conv_dim} for layer {name} not in [1,2,3]")
+        raise ValueError(f"conv_dim {conv_dim} for layer {name} not in [2,3]")
 
-    g_logger.info("{}".format(fmt_tensor_info(out)))
-    logger.debug("[End] building: {}".format(name))
+    g_logger.info(f"{fmt_tensor_info(out)}")
+    logger.debug(f"[End] building: {name}")
     return out
