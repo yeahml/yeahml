55559b97defd1616b035d302528ebfe4e4db2bd2
Jack Burdick <jackbburdick@gmail.com>
2019-08-06 19:47:01 -0600

    Update initializer logic
    I'm not thrilled with this logic overall. Initializers are either classes or functions in tf2 and I opted to implement the classes. I am still not sure how this will work with custom initializers and will likely have to revisit this logic -- especially since I am using the `get_config()` to update parameters.

--- yeahml/build/build_layers.py
+++ yeahml/build/build_layers.py
@@ -16,9 +16,15 @@ from yeahml.build.layers.other import (
 )
 
 from yeahml.build.components.regularizer import get_regularizer_fn
-from yeahml.build.components.config import return_activation, return_regularizer
+from yeahml.build.components.config import (
+    return_activation,
+    return_regularizer,
+    # return_available_initializers,
+    return_initializer,
+)
 import inspect
 
+import sys
 import types
 import functools
 
@@ -81,6 +87,37 @@ def _configure_regularizer(opt_dict):
     return reg_fn
 
 
+def _configure_initializer(opt_dict):
+    # NOTE: I'm not thrilled with the initializer logic. the core issue is that
+    # w/in tf2 there are both functions and classes implementing optimizers and
+    # I'm not sure which I want to implement (both?) and have decided to implement
+    # the class version --- however, I will need to think about how to implement
+    # custom functions here
+
+    try:
+        cur_type = opt_dict["type"]
+    except TypeError:
+        # TODO: could include more helpful message here if the type is an initializer option
+        raise TypeError(
+            f"config for initialier does not specify a 'type'. Current specified options:({opt_dict})."
+        )
+    init_obj = return_initializer(cur_type.lower())
+    init_fn = init_obj["function"]
+    if not set(opt_dict["options"].keys()).issubset(init_obj["func_args"]):
+        raise ValueError(
+            f"options {opt_dict['options'].keys()} not in {init_obj['func_args']}"
+        )
+
+    # arg_order = vars(init_fn)["__init__"].__code__.co_varnames
+    # default_values = vars(init_fn)["__init__"].__defaults__
+    cur_config = init_fn().get_config()
+    for k, v in opt_dict["options"].items():
+        cur_config[k] = v
+    out = init_fn.from_config(cur_config)
+
+    return out
+
+
 def build_layer(ltype, opts, l_name, logger, g_logger):
 
     # TODO: could place a ltype = get_ltype_mapping() here
@@ -98,24 +135,39 @@ def build_layer(ltype, opts, l_name, logger, g_logger):
         if opts:
             # TODO: encapsulate this logic, expand as needed
             # could also implement a check upfront to see if the option is valid
+            # functions to configure
             for o in opts:
                 try:
-                    if o == "kernel_regularizer":
+                    if (
+                        o == "kernel_regularizer"
+                        or o == "bias_regularizer"
+                        or o == "activity_regularizer"
+                    ):
                         opts[o] = _configure_regularizer(opts[o])
                     elif o == "activation":
                         opts[o] = _configure_activation(opts[o])
-                    elif o == "kernel_initializer":
-                        opts[o] = get_initializer_fn(opts[o])
-                    elif o == "bias_initializer":
-                        opts[o] = get_initializer_fn(opts[o])
+                    elif o == "kernel_initializer" or o == "bias_initializer":
+                        opts[o] = _configure_initializer(opts[o])
                 except ValueError as e:
                     raise ValueError(
+                        f"error creating option {o} for layer {l_name}:\n > {e}"
+                    )
+                except TypeError as e:
+                    raise TypeError(
                         f"error creating option {o} for layer {l_name}:\n > {e}"
                     )
             cur_layer = func(**opts, name=l_name)
         else:
             cur_layer = func(name=l_name)
         g_logger.info(f"{fmt_tensor_info(cur_layer)}")
+
+    # inits = return_available_initializers()
+    # for v in inits:
+    #     print(v)
+    #     print(inits[v])
+    #     print("--")
+    # # print(inits.keys())
+    # sys.exit()
 
     return cur_layer
 
--- yeahml/build/components/config.py
+++ yeahml/build/components/config.py
@@ -42,7 +42,7 @@ def return_available_activations():
     for opt_name, opt_func in available_keras_activations.items():
         if callable(opt_func):
             # TODO: likely a better way to handle this.. but I'm unsure how personally
-            if opt_name not in set(["deserialize", "get", "serialize"]):
+            if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
                 ACTIVATION_FUNCTIONS[opt_name.lower()] = {}
                 ACTIVATION_FUNCTIONS[opt_name.lower()]["function"] = opt_func
                 args = list(opt_func.__code__.co_varnames)
@@ -70,7 +70,7 @@ def return_available_regularizers():
 
     for opt_name, opt_func in available_keras_regularizers.items():
         if callable(opt_func) and not inspect.isclass(opt_func):
-            if opt_name not in set(["deserialize", "get", "serialize"]):
+            if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
                 REGULARIZER_FUNCTIONS[opt_name.lower()] = {}
                 REGULARIZER_FUNCTIONS[opt_name.lower()]["function"] = opt_func
                 args = list(opt_func.__code__.co_varnames)
@@ -89,3 +89,49 @@ def return_regularizer(regularizer_str):
         )
 
     return regularizer
+
+
+def return_available_initializers():
+
+    # I don't feel great about this logic
+
+    INITIALIZER_FUNCTIONS = {}
+    available_keras_initializers = tf.keras.initializers.__dict__
+
+    # print(available_keras_initializers.keys())
+
+    for opt_name, opt_func in available_keras_initializers.items():
+        if inspect.isclass(opt_func) and issubclass(
+            opt_func, tf.keras.initializers.Initializer
+        ):  # callable(opt_func):  # or
+            if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
+                INITIALIZER_FUNCTIONS[opt_name.lower()] = {}
+                INITIALIZER_FUNCTIONS[opt_name.lower()]["function"] = opt_func
+                try:
+                    # args = opt_func().get_config()
+                    args = list(vars(opt_func)["__init__"].__code__.co_varnames)
+                    args = [a for a in args if a != "self"]
+                except KeyError:
+                    args = None
+
+                INITIALIZER_FUNCTIONS[opt_name.lower()]["func_args"] = args
+        # elif callable(opt_func):
+        #     if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
+        #         INITIALIZER_FUNCTIONS[opt_name.lower()] = {}
+        #         INITIALIZER_FUNCTIONS[opt_name.lower()]["function"] = opt_func
+        #         args = list(opt_func.__code__.co_varnames)
+        #         INITIALIZER_FUNCTIONS[opt_name.lower()]["func_args"] = args
+
+    return INITIALIZER_FUNCTIONS
+
+
+def return_initializer(initializer_str):
+    avail_initializers = return_available_initializers()
+    try:
+        initializer = avail_initializers[initializer_str]
+    except KeyError:
+        raise KeyError(
+            f"activation {initializer_str} not available in options {avail_initializers.keys()}"
+        )
+
+    return initializer
