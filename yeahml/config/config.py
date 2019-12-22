e4a2ce15caec53979738211640c9df9cbe2ad629
Jack Burdick <jackbburdick@gmail.com>
2019-12-22 11:25:22 -0800

    change new model api
    now the new model will receive/create a copy of the existing config. This means each model will have it's own complete configuration

--- yeahml/config/config.py
+++ yeahml/config/config.py
@@ -2,7 +2,12 @@ from yeahml.config.helper import get_raw_dict_from_string
 from yeahml.config.model.parse_model import format_model_config
 
 
-def config_model(model_string: str, meta_dict: dict):
+def config_model(model_string: str, config: dict):
     model_config_raw = get_raw_dict_from_string(model_string)
-    formatted_config = format_model_config(model_config_raw, meta_dict)
-    return formatted_config
+
+    formatted_model_config = format_model_config(model_config_raw, config["meta"])
+
+    new_config = config.copy()
+    new_config["model"] = formatted_model_config
+
+    return new_config
