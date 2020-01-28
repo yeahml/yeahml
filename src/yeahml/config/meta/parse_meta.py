1f547506ed5dbcfb4e3f814ff8e5735449260932
Jack Burdick <jackbburdick@gmail.com>
2020-01-27 17:59:41 -0800

    create development directory
    contain all yeahml specific information in a single directory

--- src/yeahml/config/meta/parse_meta.py
+++ src/yeahml/config/meta/parse_meta.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from yeahml.config.helper import create_exp_dir
 
@@ -6,7 +6,7 @@ from yeahml.config.helper import create_exp_dir
 def format_meta_config(raw_config):
     formatted_dict = {}
     formatted_dict["name"] = raw_config["name"]
-    formatted_dict["experiment_dir"] = raw_config["experiment_dir"]
+    formatted_dict["experiment_dir"] = raw_config["experiment_name"]
     try:
         formatted_dict["seed"] = raw_config["rand_seed"]
     except KeyError:
@@ -24,13 +24,17 @@ def format_meta_config(raw_config):
         # no params will be loaded from previously trained params
         pass
 
-    # DEV_DIR is a hardcoded value for the directory in which the examples
-    # are located. for packing+, this will need to be removed.
-    DEV_DIR = "examples"
-    # save_dir is a hardcoded value that must match the created dir in
-    # create_standard_dirs
-    formatted_dict["log_dir"] = os.path.join(
-        ".", DEV_DIR, raw_config["name"], formatted_dict["experiment_dir"]
+    # directory for which yeahml should save information
+    try:
+        yml_dir = raw_config["yeahml_dir"]
+    except KeyError:
+        # no params will be loaded from previously trained params
+        yml_dir = "yeahml"
+
+    formatted_dict["log_dir"] = (
+        Path(yml_dir)
+        .joinpath(raw_config["name"])
+        .joinpath(formatted_dict["experiment_dir"])
     )
     # wipe is set to True for now
     create_exp_dir(formatted_dict["log_dir"], wipe_dirs=False)
