e9e9f0e45a4b117a52ebd2febb4e487894a5d0f3
Jack Burdick <jackbburdick@gmail.com>
2020-02-24 19:43:53 -0800

    begin static analysis

--- src/yeahml/config/create_configs.py
+++ src/yeahml/config/create_configs.py
@@ -38,7 +38,7 @@ def maybe_extract_from_path(cur_dict: dict) -> dict:
     return cur_dict
 
 
-def create_configs(main_path: str) -> dict:
+def primary_config(main_path: str) -> dict:
     main_config_raw = get_raw_dict_from_string(main_path)
     cur_keys = main_config_raw.keys()
     invalid_keys = []
@@ -79,5 +79,129 @@ def create_configs(main_path: str) -> dict:
         else:
             raise ValueError(f"config type {config_type} is not yet implemented")
         config_dict[config_type] = formatted_config
+
+    return config_dict
+
+
+class NOTDEFINED:
+    def __init__(self):
+        pass
+
+
+class RAW:
+    def __init__(self):
+        pass
+
+
+class g_node:
+    def __init__(
+        self,
+        name=NOTDEFINED,
+        source=NOTDEFINED,
+        in_name=NOTDEFINED,
+        in_source=NOTDEFINED,
+        out_name=[],
+    ):
+        self.name = name
+        self.source = source
+        self.in_name = in_name
+        self.in_source = in_source
+        self.out_name = out_name
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+    def __call__(self):
+        return self.conf_dict
+
+
+LOOP_ORDER = [("data", ["data", "in"]), ("model", ["model", "layers"])]
+
+
+def _extract_raw_nodes(source, cur_config):
+    cur_dict = {}
+    for k, d in cur_config.items():
+        cur_dict[k] = g_node(name=k, source=source)
+    return cur_dict
+
+
+def _obtain_nested_dict(nested, outter_dict) -> dict:
+    cur_config = None
+    for i, v in enumerate(nested):
+        if i == 0:
+            cur_config = outter_dict[v]
+        else:
+            cur_config = cur_config[v]
+    return cur_config
+
+
+def _build_skeleton(config_dict):
+    # build skeleton graph
+
+    graph_dict = {}
+    for name, nested in LOOP_ORDER:
+        raw_node_config = _obtain_nested_dict(nested, config_dict)
+        empty_node_dict = _extract_raw_nodes(name, raw_node_config)
+        graph_dict[name] = empty_node_dict
+
+    # data
+    # data_dict = {}
+    # for k, d in config_dict["data"]["in"].items():
+    #     # k = name of data, d = config spec [shape, dtype]
+    #     data_dict[k] = g_node(name=k, in_name=RAW, in_source=RAW)
+    # graph_dict["data"] = data_dict
+
+    # preprocessing
+
+    # layers
+    # layers_dict = {}
+    # for k, d in config_dict["model"]["layers"].items():
+    #     # k = name of layer
+    #     d_in = d["layer_in_name"]
+    #     layers_dict[k] = g_node(name=k)
+    # graph_dict["layers"] = data_dict
+
+    return graph_dict
+
+
+# def _validate_inputs(graph_dict: dict, config_dict: dict):
+#     for name, _ in LOOP_ORDER:
+#         cur_dict = graph_dict[name]
+#         for name, node in cur_dict:
+
+
+def static_analysis(config_dict: dict) -> dict:
+    # There's a lot that could be done here.. but for now, I think just a check
+    # to ensure inputs/outputs are specified
+
+    # build dictionary of all nodes in graph
+    graph_dict = _build_skeleton(config_dict)
+
+    # validate that all input layers are accounted for
+    # graph_dict = graph_dict()
+
+    # loop layers
+    # for l_name, d in graph_dict["layers"].items():
+    #     in_name = d["in"]
+
+    # for l_name, d in graph_dict["layers"].items():
+    #     if issubclass(d["in"], RAW):
+    #         pass
+    #     elif issubclass(d["in"], NOTDEFINED):
+    #         pass
+
+    print(graph_dict)
+
+    return {}
+
+
+def create_configs(main_path: str) -> dict:
+
+    # parse individual configs
+    config_dict = primary_config(main_path)
+
+    # validate graph
+    static_dict = static_analysis(config_dict)
+    config_dict["static"] = static_dict
 
     return config_dict
