23511697e66629cec19b4dbbe5697fe0404b9e6e
Jack Burdick <jackbburdick@gmail.com>
2020-05-24 15:20:11 -0700

    disallow duplicate keys from yml
    the was a silent error where a duplicate key would silently overwrite the first key -- this was first discovered in the `layers` where a second layer (with the same name as an earlier layer) could cause a graph construction error. will also need to include this for json

--- src/yeahml/config/helper.py
+++ src/yeahml/config/helper.py
@@ -3,6 +3,54 @@ import os
 from io import StringIO
 
 import yaml
+from yaml.constructor import ConstructorError
+from yaml.nodes import MappingNode
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+class UniqueKeyLoader(Loader):
+    # https://gist.github.com/pypt/94d747fe5180851196eb#gistcomment-2084028
+    def construct_mapping(self, node, deep=False):
+        if not isinstance(node, MappingNode):
+            raise ConstructorError(
+                None,
+                None,
+                "expected a mapping node, but found %s" % node.id,
+                node.start_mark,
+            )
+        mapping = {}
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError as exc:
+                raise ConstructorError(
+                    "while constructing a mapping",
+                    node.start_mark,
+                    "found unacceptable key (%s)" % exc,
+                    key_node.start_mark,
+                )
+            # check for duplicate keys
+            if key in mapping:
+                raise ConstructorError(
+                    "while constructing a mapping",
+                    node.start_mark,
+                    "found duplicate key",
+                    key_node.start_mark,
+                    f"duplicate key: {key} -- please rename the key to something else",
+                )
+            value = self.construct_object(value_node, deep=deep)
+            mapping[key] = value
+        return mapping
+
+
+yaml.SafeLoader.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, UniqueKeyLoader.construct_mapping
+)
 
 
 def _parse_yaml_from_path(path: str) -> dict:
@@ -10,7 +58,7 @@ def _parse_yaml_from_path(path: str) -> dict:
     try:
         with open(path, "r") as stream:
             try:
-                y = yaml.load(stream, Loader=yaml.FullLoader)
+                y = yaml.load(stream, Loader=yaml.SafeLoader)
                 return y
             except yaml.YAMLError as exc:
                 print(exc)
@@ -46,7 +94,7 @@ def _is_yaml(cur_string: str) -> bool:
 # TODO: will need to implement json logic as well
 def _parse_yaml_string(ys):
     fd = StringIO(ys)
-    dct = yaml.load(fd, Loader=yaml.FullLoader)
+    dct = yaml.load(fd, Loader=yaml.SafeLoader)
     return dct
 
 
