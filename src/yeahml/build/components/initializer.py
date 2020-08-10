50b57c898fb4a77eda57937c389c08a85285fc23
Jack Burdick <jackbburdick@gmail.com>
2020-08-10 11:42:23 -0700

    update initizalizer

--- src/yeahml/build/components/initializer.py
+++ src/yeahml/build/components/initializer.py
@@ -3,38 +3,6 @@ import inspect
 import tensorflow as tf
 
 
-def _configure_initializer(opt_dict):
-    # NOTE: I'm not thrilled with the initializer logic. the core issue is that
-    # w/in tf2 there are both functions and classes implementing optimizers and
-    # I'm not sure which I want to implement (both?) and have decided to implement
-    # the class version --- however, I will need to think about how to implement
-    # custom functions here
-
-    try:
-        cur_type = opt_dict["type"]
-    except TypeError:
-        # TODO: could include more helpful message here if the type is an initializer option
-        raise TypeError(
-            f"config for initialier does not specify a 'type'. Current specified options:({opt_dict})."
-        )
-    init_obj = return_initializer(cur_type.lower())
-    init_fn = init_obj["function"]
-    if not set(opt_dict["options"].keys()).issubset(init_obj["func_args"]):
-        raise ValueError(
-            f"options {opt_dict['options'].keys()} not in {init_obj['func_args']}"
-        )
-
-    # arg_order = vars(init_fn)["__init__"].__code__.co_varnames
-    # default_values = vars(init_fn)["__init__"].__defaults__
-    cur_config = init_fn().get_config()
-    # TODO: case were options are None?
-    for k, v in opt_dict["options"].items():
-        cur_config[k] = v
-    out = init_fn().from_config(cur_config)
-
-    return out
-
-
 def configure_initializer(func_type, func_opt):
     # NOTE: This returns init(), not init .. which is different than the regularizer
     init_obj = return_initializer(func_type)
@@ -71,13 +39,8 @@ def return_available_initializers():
             if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
                 INITIALIZER_FUNCTIONS[opt_name.lower()] = {}
                 INITIALIZER_FUNCTIONS[opt_name.lower()]["function"] = opt_func
-                try:
-                    # args = opt_func().get_config()
-                    args = list(vars(opt_func)["__init__"].__code__.co_varnames)
-                    args = [a for a in args if a != "self"]
-                except KeyError:
-                    args = None
-
+                args = inspect.signature(opt_func).parameters
+                args = list(args)
                 INITIALIZER_FUNCTIONS[opt_name.lower()]["func_args"] = args
 
     return INITIALIZER_FUNCTIONS
