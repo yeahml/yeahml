864511122ba992c41bdcf54ae2aa217aa3b77c92
Jack Burdick <jackbburdick@gmail.com>
2020-08-10 11:52:24 -0700

    update regularizer configuration

--- src/yeahml/build/components/regularizer.py
+++ src/yeahml/build/components/regularizer.py
@@ -2,62 +2,38 @@ import inspect
 
 import tensorflow as tf
 
-from yeahml.build.components.util import copy_func
-
-
-def _configure_regularizer(opt_dict):
-    # TODO: this is dangerous.... (updating the __defaults__ like this)
-    # TODO: add a try for the "type" here .like in constraint/initializer
-    reg_fn = return_regularizer(opt_dict["type"])["function"]
-    reg_fn = copy_func(reg_fn)
-    temp_copy = opt_dict.copy()
-    _ = temp_copy.pop("type")
-    if temp_copy:
-        var_list = list(reg_fn.__code__.co_varnames)
-        cur_defaults_list = list(reg_fn.__defaults__)
-        for ao, v in temp_copy.items():
-            try:
-                arg_index = var_list.index(ao)
-                cur_defaults_list[arg_index] = v
-            except ValueError:
-                raise ValueError(f"regularizer option {ao} not in options: {var_list}")
-            # TODO: same type assertion?
-        reg_fn.__defaults__ = tuple(cur_defaults_list)
-
-    return reg_fn
-
 
 def configure_regularizer(func_type, func_opt):
-    reg_fn = return_regularizer(func_type)["function"]
+    reg_obj = return_regularizer(func_type)
 
-    # configure
-    # TODO: improve this logic
+    reg_fn = reg_obj["function"]
+    cur_config = reg_fn().get_config()
     if func_opt:
-        temp_copy = func_opt.copy()
-        var_list = list(reg_fn.__code__.co_varnames)
-        cur_defaults_list = list(reg_fn.__defaults__)
-        for ao, v in temp_copy.items():
-            try:
-                arg_index = var_list.index(ao)
-                cur_defaults_list[arg_index] = v
-            except ValueError:
-                raise ValueError(f"regularizer option {ao} not in options: {var_list}")
-            # TODO: same type assertion?
-            reg_fn.__defaults__ = tuple(cur_defaults_list)
+        if not set(func_opt.keys()).issubset(reg_obj["func_args"]):
+            raise ValueError(f"options {func_opt.keys()} not in {reg_obj['func_args']}")
+        for k, v in func_opt.items():
+            cur_config[k] = v
+        out = reg_fn().from_config(cur_config)
+    else:
+        out = reg_fn()
 
-    return reg_fn
+    return out
 
 
 def return_available_regularizers():
     REGULARIZER_FUNCTIONS = {}
     available_keras_regularizers = tf.keras.regularizers.__dict__
     for opt_name, opt_func in available_keras_regularizers.items():
-        if callable(opt_func) and not inspect.isclass(opt_func):
+        if inspect.isclass(opt_func) and issubclass(
+            opt_func, tf.keras.regularizers.Regularizer
+        ):
             if opt_name.lower() not in set(["deserialize", "get", "serialize"]):
                 REGULARIZER_FUNCTIONS[opt_name.lower()] = {}
                 REGULARIZER_FUNCTIONS[opt_name.lower()]["function"] = opt_func
-                args = list(opt_func.__code__.co_varnames)
+                args = inspect.signature(opt_func).parameters
+                args = [a for a in args if a not in ["kwargs"]]
                 REGULARIZER_FUNCTIONS[opt_name.lower()]["func_args"] = args
+
     return REGULARIZER_FUNCTIONS
 
 
