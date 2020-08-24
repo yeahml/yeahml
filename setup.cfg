2cdab333f2bd47a02c5d05802c7a109df2a38b2b
Kevin Wierman <kwierman@gmail.com>
2020-08-23 23:16:03 -0700

    initial commit

---
+++ setup.cfg
@@ -0,0 +1,11 @@
+[tool:pytest]
+flake8-ignore =
+  E111 E114
+  .git ALL
+  __pycache__ ALL
+  docs/conf.py ALL
+  old ALL
+  build ALL
+  dist ALL
+  setup.py ALL
+  tests/* ALL
--- setup.py
+++ setup.py
@@ -2,9 +2,32 @@ from setuptools import find_packages, setup
 
 setup(
     name="yeahml",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    # a|b|c|rc|alpha|beta|pre|preview
-    # major.minor[.patch[.sub]], but I'm not ready to call it 0.1 yet
     version="0.0.1a1",
+    author="Jack Burdick",
+    author_email='jburdick@gmail.com',
+    description="Configuration Markup for ML Libraries",
+    long_description="TODO: Fill in",
+    packages=find_packages(),
+    package_dir={"yeahml": "yeahml"},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'yeahml=yeahml.cli:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests',
+    zip_safe=True,
+    license="Apache license",
+    url='https://github.com/yeahml/yeahml',
+    keywords='yeahml,ml,configuration,baselining',
 )
