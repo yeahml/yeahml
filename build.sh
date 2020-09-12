47877e8d922ca19334bbb4e6ab3b16897644341f
Kevin Wierman <kwierman@gmail.com>
2020-09-12 14:14:25 -0700

    Fixed conda build

---
+++ build.sh
@@ -0,0 +1,2 @@
+pip install https://files.pythonhosted.org/packages/e9/8a/321cd8ea5f4a22a06e3ba30ef31ec33bea11a3443eeb1d89807640ee6ed4/toposort-1.5-py2.py3-none-any.whl
+pip install . --ignore-installed
