9118fe32e3304ed0a12b9f681db8f0c266b68554
Jack Burdick <jackbburdick@gmail.com>
2021-02-25 17:09:18 -0800

    Attempt to convert from function to object
    I'm not convinced this is the best approach, but at the same time, I believe this will make maintenance easier and increase readability

--- src/yeahml/__init__.py
+++ src/yeahml/__init__.py
@@ -26,7 +26,7 @@ from yeahml.config.create_configs import create_configs
 from yeahml.build.build_model import build_model
 
 # train
-from yeahml.train.train_model import train_model
+from yeahml.train.trainer import Trainer
 
 # evaluate
 from yeahml.evaluate.eval_model import eval_model
