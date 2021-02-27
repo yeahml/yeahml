30b75b53b3b96cebc2611579bd81a2c1d6079c86
Jack Burdick <jackbburdick@gmail.com>
2021-02-27 11:35:55 -0800

    Retain entire config

--- src/yeahml/train/controller/components/sub/performance.py
+++ src/yeahml/train/controller/components/sub/performance.py
@@ -1,8 +1,11 @@
+# from yeahml.build.components.loss import configure_loss
+
+
 class Performance:
     # NOTE: what if we want to keep track of metrics that don't rely on the
     # current head/output graph? (would need an outter metric)
     def __init__(self, loss_config, metric_config):
-        self.loss = loss_config["object"]  # single loss
+        self.loss = loss_config
         self.metric_dict = metric_config
 
         # losses and metrics are different in tf. that is losses don't have
